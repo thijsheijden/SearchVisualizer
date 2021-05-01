422fb4862ca4396aef119721de211d115c8574ae
thijsheijden <hi@thijsheijden.nl>
2021-05-01 12:48:36 +0200

    Cleaned up the grid drawing code

--- cmd/search-visualizer/main.go
+++ cmd/search-visualizer/main.go
@@ -23,6 +23,14 @@ var (
 	blue       = color.NRGBA{R: 0x40, G: 0x40, B: 0xC0, A: 0xFF}
 )
 
+// Grid variables
+var (
+	gridRows         int     = 10
+	gridColumns      int     = 10
+	gridRowWeight    float32 = 1 / float32(gridRows)
+	gridColumnWeight float32 = 1 / float32(gridColumns)
+)
+
 func main() {
 	go func() {
 		w := app.NewWindow(app.Size(unit.Px(1024), unit.Px(800)))
@@ -42,17 +50,7 @@ func loop(w *app.Window) error {
 			return e.Err
 		case system.FrameEvent:
 			gtx := layout.NewContext(&ops, e)
-			ncols := 10
-			weight := 1 / float32(ncols)
-			var columns []layout.FlexChild
-			for i := 0; i < ncols; i++ {
-				columns = append(columns,
-					layout.Flexed(weight, func(gtx layout.Context) layout.Dimensions {
-						return rows(gtx)
-					}),
-				)
-			}
-			layout.Flex{Axis: layout.Vertical}.Layout(gtx, columns...)
+			grid(gtx)
 			e.Frame(gtx.Ops)
 		}
 	}
@@ -68,18 +66,16 @@ func colorBox(gtx layout.Context, size image.Point, color color.NRGBA) layout.Di
 	return layout.Dimensions{Size: size}
 }
 
-func rows(gtx layout.Context) layout.Dimensions {
-	nrows := 10
-	weight := 1 / float32(nrows)
-	var rows []layout.FlexChild
-	for i := 0; i < nrows; i++ {
-		rows = append(rows,
-			layout.Flexed(weight, func(gtx layout.Context) layout.Dimensions {
+func drawRow(gtx layout.Context) layout.Dimensions {
+	var columns []layout.FlexChild
+	for i := 0; i < gridColumns; i++ {
+		columns = append(columns,
+			layout.Flexed(gridColumnWeight, func(gtx layout.Context) layout.Dimensions {
 				return inset(gtx)
 			}),
 		)
 	}
-	return layout.Flex{Axis: layout.Horizontal}.Layout(gtx, rows...)
+	return layout.Flex{Axis: layout.Horizontal}.Layout(gtx, columns...)
 }
 
 func inset(gtx layout.Context) layout.Dimensions {
@@ -87,4 +83,16 @@ func inset(gtx layout.Context) layout.Dimensions {
 	return layout.UniformInset(unit.Dp(8)).Layout(gtx, func(gtx layout.Context) layout.Dimensions {
 		return colorBox(gtx, gtx.Constraints.Max, red)
 	})
+}
+
+func grid(gtx layout.Context) layout.Dimensions {
+	var rows []layout.FlexChild
+	for i := 0; i < gridColumns; i++ {
+		rows = append(rows,
+			layout.Flexed(gridRowWeight, func(gtx layout.Context) layout.Dimensions {
+				return drawRow(gtx)
+			}),
+		)
+	}
+	return layout.Flex{Axis: layout.Vertical}.Layout(gtx, rows...)
 }
